6c878d7ea6b32e5ed60ffdda927aa87157eb4b0a
njgarg22 <njgarg22@gmail.com>
2020-10-17 12:57:40 +0530

    Comment

--- srv/src/main/java/com/sap/cap/productsservice/Application.java
+++ srv/src/main/java/com/sap/cap/productsservice/Application.java
@@ -3,6 +3,7 @@ package com.sap.cap.productsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Typical boilerplate code found in every Spring Boot application
 @SpringBootApplication
 public class Application {
 
